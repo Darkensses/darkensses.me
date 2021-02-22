e92c2be3a05fcf48201476ecb5aa51e1fab824e7
Jasiel Guillen <darkensses@gmail.com>
2021-02-22 01:18:50 -0600

    First iteration of this new version :zap:

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "./layoutv2.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,12 +25,12 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: 1920,
+          padding: `0 4.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
