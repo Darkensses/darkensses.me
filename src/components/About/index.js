b19f4308c9b8bdf0a0e5a1df95d7ec1ba4f3ba7a
Jasiel Guillen <darkensses@gmail.com>
2021-04-06 23:04:17 -0500

    Tiny fixes :taco:

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,37 +16,42 @@ function About() {
     }
   `)
   return (
-    <section id="about" className="about__section">        
-      <div className="about__wrapper">
-      <h2 style={{width: '100%', position: 'relative'}}>Sobre Mí</h2>
-        <div
-          style={{
-            position: "relative",
-            width: '100%',
-            maxWidth: "300px",
-            maxHeight: "300px",
-          }}
-        >
-          <Img
-            className="about__img"
-            fluid={data.fileName.childImageSharp.fluid}
-            alt="Avatar"
-          />
-        </div>
-        <div style={{marginLeft: 30, marginRight: 30}}>
-          <p style={{marginBottom: 15}}>
-            ¡Hey! me llamo Jasiel, me gusta escuchar a los King Gizzard {"&"}{" "}
-            the Lizard Wizard y desarrollar cosas para automatizar pruebas y
-            procesos, tambíen por diversión y para hacer un poco de ROM Hacking
-            para PSX :)
-          </p>
-          <p>
-            Actualmente trabajo como SDET en el area de QA @ Softtek. Tambien
-            soy mentor porque me gusta difundir y compartir con todo el mundo lo
-            que he aprendido. Creo fielmente en el Open Source y soy un
-            entusiasta de los Hackathons, tanto que he partcipado como Juez en
-            el HackMTY, el hackathon más grande de latinoamérica.
-          </p>
+    <section id="about">        
+      <div className="about__section">
+        <div className="about__wrapper">
+        
+          <div className="about__content">
+            <div
+              style={{
+                position: "relative",
+                width: '100%',
+                maxWidth: "300px",
+                maxHeight: "300px",
+              }}
+            >
+              <Img
+                className="about__img"
+                fluid={data.fileName.childImageSharp.fluid}
+                alt="Avatar"
+              />
+            </div>
+            <div style={{marginLeft: 30, marginRight: 30, fontSize: '14px', lineHeight: 1.8, textAlign: 'justify', textJustify: 'inter-word'}}>
+              <h2 style={{lineHeight: 2, marginBottom: 15}}>Sobre Mí</h2>
+              <p style={{marginBottom: 15}}>
+                ¡Hey! me llamo Jasiel, me gusta escuchar a los King Gizzard {"&"}{" "}
+                the Lizard Wizard y desarrollar cosas para automatizar pruebas y
+                procesos, tambíen por diversión y para hacer un poco de ROM Hacking
+                para PSX :)
+              </p>
+              <p>
+                Actualmente trabajo como SDET en el area de QA @ Softtek. Tambien
+                soy mentor porque me gusta difundir y compartir con todo el mundo lo
+                que he aprendido. Creo fielmente en el Open Source y soy un
+                entusiasta de los Hackathons, tanto que he participado como Juez en
+                el HackMTY, el hackathon más grande de latinoamérica.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
