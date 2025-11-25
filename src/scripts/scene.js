ea622c89500d230f8fae5c0321ae632f0ad3b133
Jasiel Guillén <darkensses@gmail.com>
2025-11-24 22:02:48 -0600

    feat: add image of my username as a texture :)))

--- src/scripts/scene.js
+++ src/scripts/scene.js
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
-import adreamShader from '../assets/glsl/adream.glsl'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import handGLB from '../assets/models/hand.glb';
+
+import imgUsername from '../assets/images/text.png'
 
 const canvas = document.getElementById('webgl');
 const scene = new THREE.Scene();
@@ -25,6 +24,7 @@ window.addEventListener('resize', () => {
 const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+//renderer.autoClear = false;
 
 /**
  * Camera
@@ -36,24 +36,31 @@ scene.add(camera);
 /**
  * Scene 😎
  */
-const geometry = new THREE.PlaneGeometry(5, 5)
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const shaderMaterial = new THREE.ShaderMaterial({
-  uniforms: {
-    u_time: { value: 1.0 },
-  },
-  vertexShader: `
-  varying vec2 vUv;
-  void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-  }
-  `,
-  fragmentShader: adreamShader,
-  transparent: true
-})
-const mesh = new THREE.Mesh(geometry, shaderMaterial);
-scene.add(mesh);
+
+
+const texture = new THREE.TextureLoader().load(imgUsername.src, () => {
+  console.log('textureee');
+  //texture.minFilter = THREE.NearestFilter;
+  //texture.magFilter = THREE.NearestFilter;
+  texture.generateMipmaps = false;
+  texture.colorSpace = THREE.SRGBColorSpace;
+  const w = texture.image.width;
+  const h = texture.image.height;
+
+  console.log(w,h)
+  const geometry = new THREE.PlaneGeometry(10,10/(w/h));  
+
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+    transparent: true,
+    depthWrite: true
+  });
+
+  const plane = new THREE.Mesh(geometry, material);
+  plane.position.set(0, 0, 0);
+  //plane.layers.set(1)
+  scene.add(plane);
+});
 
 /**
  * Lights 💡
@@ -61,49 +68,10 @@ scene.add(mesh);
 const light = new THREE.AmbientLight(0xffffff, 20); // soft white light
 scene.add(light);
 
-/**
- * Model
- */
-let mixer;
-const loader = new GLTFLoader();
-
-loader.load(handGLB, (glb) => {
-	const model = glb.scene;
-	model.position.set(3,-2,0)
-  model.scale.setX(-1);
-	scene.add(model);
-
-  model.traverse((child) => {
-    if(child.isMesh) {
-      child.material = new THREE.MeshBasicMaterial({
-        color: 0x0000ff,
-        wireframe: true
-      });
-    }
-  })
-
-	mixer = new THREE.AnimationMixer(model);
-
-	const action = mixer.clipAction(glb.animations[0]);
-	//action.play();
-
-},
-undefined,
-(error) => {
-	console.error(error);
-})
 
 const clock = new THREE.Clock();
 const animate = () => {
   window.requestAnimationFrame(animate);
-  const delta = clock.getDelta();
-
-  shaderMaterial.uniforms.u_time.value = clock.getElapsedTime();
-
-  if(mixer) {
-  	mixer.update(delta)
-  }
-
   renderer.render(scene, camera);
 };
 
