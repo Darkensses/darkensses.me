085ebf13e89ffe46921eadd34763837ab87c51fd
Jasiel Guillén <darkensses@gmail.com>
2026-01-04 22:43:47 -0600

    feat: scene prototype finished

--- src/scripts/scene.js
+++ src/scripts/scene.js
@@ -1,27 +1,41 @@
 import * as THREE from 'three';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
-import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js'
-import { CGAShader } from '../scripts/fx/FxCGA';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EffectComposer, FXAAShader, OutputPass, RenderPass, RGBShiftShader, ShaderPass, UnrealBloomPass } from 'three/examples/jsm/Addons.js';
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+import { CGAShader } from './fx/FxCGA';
 import { BadTVShader } from './shaders/BadTVShader';
+
+import GUI from 'lil-gui';
 
 import imgUsername from '../assets/images/text.png'
 import coolvertex from '../scripts/shaders/coolvertex.vert'
 import simplecolorFrag from '../scripts/shaders/simplecolor.frag'
 
 const canvas = document.getElementById('webgl');
-const scene = new THREE.Scene();
-const sizes = { width: canvas.offsetWidth, height: canvas.offsetHeight};
+const sizes = { width: window.innerWidth, height: window.innerHeight };
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.autoClear = false; // set to false if you want to use layers ;)
+renderer.autoClear = false;
+
+/**
+ * Sizes
+ */
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();  
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+  composer.setSize(sizes.width, sizes.height);
+  finalComposer.setSize(sizes.width, sizes.height);
+});
 
 /**
  * Camera
@@ -30,62 +44,38 @@ const perspective = 600;
 const fov = 2*Math.atan((sizes.height/2)/perspective) * (180/Math.PI);
 const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 1, 1000);
 camera.position.z = perspective;
-//scene.add(camera);
 
 /**
- * Post processing
+ * HTML Elements
  */
-
-// Effect composer
-const effectComposer = new EffectComposer(renderer)
-effectComposer.setSize(sizes.width, sizes.height)
-effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// Render pass
-const renderPass = new RenderPass(scene, camera)
-effectComposer.addPass(renderPass)
-
-const badTVPass = new ShaderPass(BadTVShader);
-badTVPass.uniforms.distortion.value = 0.1;
-badTVPass.uniforms.distortion2.value = 0.2;
-badTVPass.uniforms.rollSpeed.value = 0;
-effectComposer.addPass(badTVPass);
-
-const cgaPass = new ShaderPass(CGAShader);
-cgaPass.uniforms.resolution.value.set(sizes.width,sizes.height)
-cgaPass.uniforms.colDark.value = new THREE.Color('#0000ff');
-cgaPass.uniforms.colLight.value = new THREE.Color('#00a1ff');
-cgaPass.uniforms.amount.value   = 1;
-cgaPass.uniforms.scale.value    = 3;
-effectComposer.addPass(cgaPass);
-
-// rgb shift pass
-const rgbShiftPass = new ShaderPass(RGBShiftShader)
-rgbShiftPass.uniforms['amount'].value = 0.002
-rgbShiftPass.renderToScreen = true
-rgbShiftPass.enabled = true
-effectComposer.addPass(rgbShiftPass)
-
-const bloomPass = new UnrealBloomPass( 
-  new THREE.Vector2(sizes.width, sizes.height), 
-0.6, 0.1, 0.6 
-);
-effectComposer.addPass( bloomPass );
-
-/**
- * Scene 😎
- */
-
 const logotext = document.getElementById('logotext');
 const bounds = logotext.getBoundingClientRect();
-console.log(bounds)
 
-const geometry = new THREE.PlaneGeometry(bounds.width,bounds.height,1);
-const material = new THREE.MeshBasicMaterial({color: 'red', wireframe: true});
-const mesh = new THREE.Mesh(geometry, material);
-mesh.position.x = bounds.left - sizes.width/2 + bounds.width/2;
-mesh.position.y = -bounds.top + sizes.height/2 - bounds.height/2;
-//scene.add(mesh)
+const scene = new THREE.Scene();
+
+const texture = new THREE.TextureLoader().load(imgUsername.src, () => {
+  texture.minFilter = THREE.LinearFilter;
+  texture.colorSpace = THREE.SRGBColorSpace;
+  texture.generateMipmaps = false;
+
+  const w = texture.image.width;
+  const h = texture.image.height;
+  const aspect = w/h;
+
+  const geometry = new THREE.PlaneGeometry(texture.image.height*aspect,texture.image.height,1);
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+    transparent: true,
+    depthWrite: false,
+  });
+
+  const plane = new THREE.Mesh(geometry, material);
+  plane.position.x = bounds.left - sizes.width/2 + bounds.width/2;
+  plane.position.y = -bounds.top + sizes.height/2 - bounds.height/2;
+  plane.layers.set(1);
+  scene.add(plane);
+});
+
 
 const coolGeometry = new THREE.SphereGeometry(280,40,40)
 const coolMaterial = new THREE.ShaderMaterial({
@@ -99,79 +89,119 @@ const coolMaterial = new THREE.ShaderMaterial({
 });
 const coolMesh = new THREE.Mesh(coolGeometry, coolMaterial);
 coolMesh.position.z = -300;
-coolMesh.layers.set(1);
+coolMesh.layers.set(0);
 scene.add(coolMesh);
 
-let textureReady = false;
-const texture = new THREE.TextureLoader().load(imgUsername.src, () => {
-  console.log('textureee');
-  //texture.minFilter = THREE.NearestFilter;
-  //texture.magFilter = THREE.NearestFilter;
-  texture.minFilter = THREE.LinearFilter;
-  texture.generateMipmaps = false;
-  texture.colorSpace = THREE.SRGBColorSpace;
-  const w = texture.image.width;
-  const h = texture.image.height;
-
-  const aspect = w/h;
-
-  console.log(w,h)  
-  const geometry = new THREE.PlaneGeometry(texture.image.height*aspect,texture.image.height,1);  
-
-  const material = new THREE.MeshBasicMaterial({
-    map: texture,
-    transparent: true,
-    depthWrite: true,
-  });
-
-  const plane = new THREE.Mesh(geometry, material);
-  plane.position.x = bounds.left - sizes.width/2 + bounds.width/2;
-  plane.position.y = -bounds.top + sizes.height/2 - bounds.height/2;  
-  plane.layers.set(1)
-  scene.add(plane);
-  textureReady = true;
-});
-
 /**
- * Sizes
+ * Post FX
  */
-// const sizes = { width: window.innerWidth, height: window.innerHeight };
-window.addEventListener('resize', () => {
-  sizes.width = canvas.offsetWidth;
-  sizes.height = canvas.offsetHeight;
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const renderPass = new RenderPass(scene, camera);
+
+const cgaPass = new ShaderPass(CGAShader);
+cgaPass.uniforms.resolution.value.set(sizes.width, sizes.height)
+cgaPass.uniforms.colDark.value = new THREE.Color('#0000ff');
+cgaPass.uniforms.colLight.value = new THREE.Color('#00a1ff');
+cgaPass.uniforms.amount.value   = 1;
+cgaPass.uniforms.scale.value    = 3;
+
+const badTVPass = new ShaderPass(BadTVShader);
+badTVPass.uniforms.distortion.value = 0.1;
+badTVPass.uniforms.distortion2.value = 0.2;
+badTVPass.uniforms.rollSpeed.value = 0;
+
+const rgbShiftPass = new ShaderPass(RGBShiftShader);
+rgbShiftPass.uniforms.amount.value = 0.0015;
+rgbShiftPass.renderToScreen = true;
+rgbShiftPass.enabled = true;
+
+const bloom = new UnrealBloomPass(new THREE.Vector2(sizes.width, sizes.height), 0.2, 0.0, 0.0 );
+
+const fxaaPass = new ShaderPass( FXAAShader );
+fxaaPass.uniforms.resolution.value.set( 1 / sizes.width, 1 / sizes.height );
+fxaaPass.renderToScreen = true;
+fxaaPass.material.transparent = true; 
+
+const baseRenderTarget = new THREE.WebGLRenderTarget( sizes.width, sizes.height, { type: THREE.HalfFloatType } )
+const composer = new EffectComposer(renderer);
+composer.renderToScreen = false;
+composer.addPass(renderPass);
+composer.addPass(fxaaPass);
+composer.addPass(badTVPass);
+composer.addPass(cgaPass);
+composer.addPass(rgbShiftPass);
+composer.addPass(bloom);
+
+const mixPass = new ShaderPass(
+  new THREE.ShaderMaterial({
+    uniforms: {
+      baseTexture: { value: null },
+      fxTexture: { value: null }
+    },
+    vertexShader: `
+      varying vec2 vUv;
+			void main() {
+				vUv = uv;
+				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+			}
+    `,
+    fragmentShader: `
+      varying vec2 vUv;
+
+      uniform sampler2D baseTexture;
+      uniform sampler2D fxTexture;
+
+      void main() {
+        gl_FragColor = texture2D(baseTexture, vUv) + texture2D(fxTexture, vUv);
+      }
+    `
+  })
+);
+mixPass.needsSwap = true;
+
+const outputPass = new OutputPass();
+
+const finalComposer = new EffectComposer(renderer);
+finalComposer.addPass(renderPass);
+finalComposer.addPass(mixPass);
+finalComposer.addPass(outputPass);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const gui = new GUI();
+const fxGUI = { 
+  rollSpeed: 1
+}
+gui.add(fxGUI, 'rollSpeed', -3,3,0.1).onChange(function (value) {
+  console.log(value);
+  badTVPass.uniforms.rollSpeed.value = value;
 });
-
-/**
- * Lights 💡
- */
-const light = new THREE.AmbientLight(0xffffff, 20); // soft white light
-scene.add(light);
-
 
 const clock = new THREE.Clock();
-const animate = () => {
-  coolMaterial.uniforms.uTime.value += 0.05;
-
+const animate = () => {  
   window.requestAnimationFrame(animate);
+  //let delta = clock.getDelta();
+  //const delta = performance.now() * 0.001 + 6000;
 
-  if(!textureReady) return;
+  coolMaterial.uniforms.uTime.value += 0.05;
+  badTVPass.uniforms.time.value = clock.getElapsedTime() * 0.05;
+  controls.update();
 
-  //renderer.render(scene, camera);
-
-  // https://discourse.threejs.org/t/solved-effectcomposer-layers/3158/4
-  renderer.clear();
-  
-  badTVPass.uniforms.time.value = clock.getElapsedTime() * 0.02;
-  camera.layers.set(1);
-  effectComposer.render();
-  
-  renderer.clearDepth();
   camera.layers.set(0);
-  renderer.render(scene, camera)
-};
+  renderer.setRenderTarget(baseRenderTarget);
+  renderer.clear(true, true, true);
+  renderer.render(scene, camera);
+  
+  camera.layers.set(1);
+  composer.render();
 
+  mixPass.uniforms.baseTexture.value = baseRenderTarget.texture;
+  mixPass.uniforms.fxTexture.value = composer.readBuffer.texture; // result of FX chain
+  renderer.setRenderTarget(null);
+
+  finalComposer.render();
+  stats.update();
+  //renderer.render(scene, camera);
+}
 animate();
