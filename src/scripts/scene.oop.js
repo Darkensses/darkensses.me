acee903e5a91fec8369cae5f9d8732c20e26edb3
Jasiel Guillén <darkensses@gmail.com>
2026-01-24 00:45:29 -0600

    feat: add the FX chain and a grid :cool:

--- src/scripts/scene.oop.js
+++ src/scripts/scene.oop.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import Lenis from 'lenis';
 
 import imgLogo from '../assets/images/text.png';
-import { EffectComposer, FXAAShader, RenderPass, RGBShiftShader, ShaderPass, UnrealBloomPass } from 'three/examples/jsm/Addons.js';
+import { EffectComposer, FXAAShader, OutputPass, RenderPass, RGBShiftShader, ShaderPass, UnrealBloomPass } from 'three/examples/jsm/Addons.js';
 import { CGAShader } from './fx/FxCGA';
 import { BadTVShader } from './shaders/BadTVShader';
 
@@ -26,6 +26,7 @@ export default class MainScreen {
     this.initCamera();
     this.initRenderer();
     //this.initMesh();
+    this.initGrid();
     this.addLogo();
     this.initFX();
     this.addEventListeners();
@@ -63,6 +64,7 @@ export default class MainScreen {
 
       // If the DOM element moves with scroll, keep the plane glued to it
       this.syncLogoToDOM();
+      this.onScrollGrid(this.lenis.actualScroll)
     });
   }
 
@@ -85,7 +87,28 @@ export default class MainScreen {
     const geometry = new THREE.BoxGeometry(200, 200, 200);
     const material = new THREE.MeshBasicMaterial({ color: "#7444ff" });
     this.mesh = new THREE.Mesh(geometry, material);
+    this.mesh.layers.set(0);
     this.scene.add(this.mesh);
+  }
+
+  initGrid() {
+    this.gridHelper = new THREE.GridHelper(1000,16, 0x0000FF, 0x0000FF);
+    this.gridHelper.position.z = -50;
+    this.gridHelper.rotation.x = 10 * Math.PI / 180;
+    this.scene.add(this.gridHelper);
+  }
+
+  onScrollGrid(value) {
+    //const bounds = this.logoDom.getBoundingClientRect();
+
+    // Because the FOV trick, world units at z=0 map 1:1 to CSS pixels.
+    //this.planeLogo.scale.set(bounds.width, bounds.height, 1);
+
+    // this.planeLogo.position.x = bounds.left - this.sizes.width / 2 + bounds.width / 2;
+    // this.planeLogo.position.y = -bounds.top + this.sizes.height / 2 - bounds.height / 2;
+    
+    //this.gridHelper.rotation.x = (10 * Math.PI / 180) + ((value*0.1) * Math.PI / 180);
+    this.gridHelper.position.z = -value*0.5;
   }
 
   applyCoverUV(texture, planeW, planeH) {
@@ -121,6 +144,8 @@ export default class MainScreen {
     this.planeLogo.position.x = bounds.left - this.sizes.width / 2 + bounds.width / 2;
     this.planeLogo.position.y = -bounds.top + this.sizes.height / 2 - bounds.height / 2;
 
+    this.gridHelper.position.y = this.planeLogo.position.y*0.85;
+
     this.applyCoverUV(this.logoTexture, bounds.width, bounds.height);
   }
 
@@ -145,6 +170,8 @@ export default class MainScreen {
       this.planeLogo = new THREE.Mesh(geometry, material);
       this.planeLogo.position.z = 0; // Important for the 1:1 mapping assumption
 
+      this.planeLogo.layers.set(1);
+
       this.scene.add(this.planeLogo);
 
       this.syncLogoToDOM();
@@ -152,8 +179,9 @@ export default class MainScreen {
   }
 
   initFX() {
+    this.baseRenderTarget = new THREE.WebGLRenderTarget( this.sizes.width, this.sizes.height, { type: THREE.HalfFloatType } )
     this.composer = new EffectComposer(this.renderer);
-    //this.composer.renderToScreen = false;
+    this.composer.renderToScreen = false;
     const renderPass = new RenderPass(this.scene, this.camera);
 
     const fxaaPass = new ShaderPass(FXAAShader);
@@ -186,15 +214,62 @@ export default class MainScreen {
     this.composer.addPass(this.badTVPass);
     this.composer.addPass(cgaPass);
     this.composer.addPass(rgbShiftPass);
-    this.composer.addPass(bloom);   
+    this.composer.addPass(bloom);
+
+    this.mixPass = new ShaderPass(
+      new THREE.ShaderMaterial({
+        uniforms: {
+          baseTexture: { value: null },
+          fxTexture: { value: null }
+        },
+        vertexShader: `
+          varying vec2 vUv;
+          void main() {
+            vUv = uv;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+          }
+        `,
+        fragmentShader: `
+          varying vec2 vUv;
+
+          uniform sampler2D baseTexture;
+          uniform sampler2D fxTexture;
+
+          void main() {
+            gl_FragColor = texture2D(baseTexture, vUv) + texture2D(fxTexture, vUv);
+          }
+        `
+      })
+    );
+    this.mixPass.needsSwap = true;
+
+    const outputPass = new OutputPass();
+    this.finalComposer = new EffectComposer(this.renderer);
+    this.finalComposer.addPass(renderPass);
+    this.finalComposer.addPass(this.mixPass);
+    this.finalComposer.addPass(outputPass);
   }
 
   animate() {
     window.requestAnimationFrame(() => this.animate());
     //this.renderer.render(this.scene, this.camera);
     this.badTVPass.uniforms.time.value = this.clock.getElapsedTime() * 0.05;
+
+    this.camera.layers.set(0);
+    this.renderer.setRenderTarget(this.baseRenderTarget);
     this.renderer.clear();
+    this.renderer.render(this.scene, this.camera);
+
+    this.camera.layers.set(1);
     this.composer.render();
+
+    this.mixPass.uniforms.baseTexture.value = this.baseRenderTarget.texture;
+    this.mixPass.uniforms.fxTexture.value = this.composer.readBuffer.texture; // result of FX chain
+
+    this.finalComposer.render();
+
+    //this.renderer.clear();
+    //this.composer.render();
   }
 }
 
