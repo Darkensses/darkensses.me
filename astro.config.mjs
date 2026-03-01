9c1483a2aa2ac591d9e3d24fdbe52e0f2bf195ef
Jasiel Guillén <darkensses@gmail.com>
2026-03-01 00:32:36 -0600

    chore(deps): add astro-tunnel to test the page in other devices

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import glsl from 'vite-plugin-glsl';
 
+import tunnel from 'astro-tunnel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [glsl()],
     assetsInclude: ['**/*.glb']
-  }
+  },
+
+  integrations: [tunnel()]
 });
